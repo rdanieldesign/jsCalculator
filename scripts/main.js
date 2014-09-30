d8353f98e3470a7cbe202ec648d8d2c5994bb74a
rdanieldesign <rdanieldesign@gmail.com>
2014-09-30 14:52:21 -0400

    Structure HTML

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,14 @@
 var num1 = document.getElementById('num_one');
 var num2 = document.getElementById('num_two');
+var answer = document.getElementById('answer');
+var button = document.getElementById('calculateBtn');
 
-
-var calculate = function(num1, num2) {
-    var answer = num1 + num2;
-    document.getElementById('answer').innerHTML = answer.toString();
+var calculate = function(x, y) {
+    var sum = x + y;
+    answer.innerHTML = sum.toString();
 }
 
-document.getElementById('calculateBtn').onclick = function(){
+button.onclick = function(){
     var box1 = Number(num1.value);
     var box2 = Number(num2.value);
     calculate(box1, box2);
